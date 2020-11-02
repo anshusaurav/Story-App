94fdddcbccf04af080f179a41bce4c0e0ab10273
anshusaurav <anshu.saurav@gmail.com>
2020-11-02 18:26:59 +0530

    Netlify Deploy

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import Home from "./components/Home"
-import "video-react/dist/video-react.css";
 class App extends Component {
   render() {
     return (
--- src/components/Loaders.js
+++ src/components/Loaders.js
@@ -24,7 +24,7 @@ export const MainLoader = () => {
     return (
         <div class="">
             {arr.map(elem => (
-                <div class="border border-gray-300 shadow rounded-md p-4 max-w-sm w-full mx-auto">
+                <div class="border border-gray-300 shadow rounded-md p-4 w-full mx-auto">
                     <div class="animate-pulse flex space-x-4">
                         <div class="rounded-full bg-gray-400 h-12 w-12"></div>
                         <div class="flex-1 space-y-4 py-1">
